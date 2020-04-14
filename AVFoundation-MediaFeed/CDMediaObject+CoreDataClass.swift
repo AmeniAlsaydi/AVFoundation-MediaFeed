f31bca7c3aa3eb8ac1edf57d0643dcec50c66745
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-04-14 12:28:52 -0400

    created an CDMediaObject entity and attributes and generated the managed object class

---
+++ AVFoundation-MediaFeed/CDMediaObject+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CDMediaObject+CoreDataClass.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Amy Alsaydi on 4/14/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CDMediaObject: NSManagedObject {
+
+}
